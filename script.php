b591a37f4e72a4e5dca0c4b1358ecdce76ed1ca3
cdellaie <cdellaie@users.noreply.github.com>
2023-09-18 09:17:54 +0200

    Init
    Webpage migration

---
+++ script.php
@@ -0,0 +1,18 @@
+<?php echo '<script>
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+</script>
+
+<!--// Scripts for LateX in html, using MathJax-->
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+  <script id="MathJax-script" async
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
+  </script>' ; ?>
